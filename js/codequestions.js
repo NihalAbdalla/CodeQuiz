ee8d4dab55cb3a8e4fcffb34389cc95300596fe8
Nihal Abdalla <miz46769@gmail.com>
2021-07-08 17:49:35 -0400

    basie css style

---
+++ js/codequestions.js
@@ -0,0 +1,23 @@
+var quizQuestions = [
+    {
+        title: "What is git used for?",
+        choices: ["storage", "file sharing", "versioning", "credential"],
+        answer: "versioning"
+    },
+    ,
+    {
+        title: "What is CSS?",
+        choices: ["Cascading Style Sheets", "Coding Style Sheets", "Class Style Sheets", "Classic Style Shade"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        title: "Where in an HTML document is the correct place to refer to an external CSS?",
+        choices: ["in the Head section", "in the Body section", "in the End of the Document"],
+        answer: "in the Head section"
+    },
+    {
+        title: "Which HTML attribute is used to define inline styles?",
+        choices: ["style", "font", "styles", "class"],
+        answer: "style"
+    }
+];
